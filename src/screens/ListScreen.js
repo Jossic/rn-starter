f1a7a0ab0dc18159d5e72849f36eb582ea5679a7
Jossic <jossic.lapierre@gmail.com>
2021-10-13 17:26:27 +0200

    Fin 21

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
+	const friends = [
+		{
+			name: 'friend #1',
+		},
+		{
+			name: 'friend #2',
+		},
+		{
+			name: 'friend #3',
+		},
+		{
+			name: 'friend #4',
+		},
+		{
+			name: 'friend #5',
+		},
+		{
+			name: 'friend #6',
+		},
+		{
+			name: 'friend #7',
+		},
+	];
 	return (
-		<View>
-			<Text></Text>
-		</View>
+		<FlatList
+			data={friends}
+			renderItem={({ item }) => {
+				return <Text>{item.name}</Text>;
+			}}></FlatList>
 	);
 };
 
