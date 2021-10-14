939a79beb6bf2ca0279689fdfd3083ca65226380
Jossic <jossic.lapierre@gmail.com>
2021-10-14 17:32:35 +0200

    Fin 46

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
+const randomRgb = () => {
+	const red = Math.floor(Math.random() * 256);
+	const green = Math.floor(Math.random() * 256);
+	const blue = Math.floor(Math.random() * 256);
+	return `rgb(${red},${green},${blue})`;
+};
+
 const ColorScreen = () => {
 	return (
 		<View>
 			<Button title='Add a color' />
+			<View
+				style={{
+					height: 100,
+					width: 100,
+					backgroundColor: randomRgb(),
+				}}></View>
 		</View>
 	);
 };
