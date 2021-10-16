43cc50df13da02aca26a3b651906b16085040e41
Jossic <jossic.lapierre@gmail.com>
2021-10-16 13:56:00 +0200

    Fin 64

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 const TextScreen = () => {
 	return (
 		<View>
 			<Text style={styles.text}>Text Screen</Text>
+			<TextInput style={styles.input}>Text Screen</TextInput>
 		</View>
 	);
 };
@@ -13,4 +14,12 @@ export default TextScreen;
 
 const styles = StyleSheet.create({
 	text: { fontSize: 25, textAlign: 'center', marginTop: 20 },
+	input: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 15,
+		borderColor: 'black',
+		borderWidth: 1,
+		borderRadius: 10,
+	},
 });
